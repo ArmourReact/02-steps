1c0bb4b628c7ef2759d733dc15b7a2ef7287bfb6
Jake Armour <github@jakearmour.com>
2023-07-11 15:44:04 +0100

    feat: event handling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ const messages = [
 const App = () => {
   const step = 1;
 
+  const handlePrevious = () => {
+    alert("previous");
+  };
+
+  const handleNext = () => {
+    alert("next");
+  };
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +28,16 @@ const App = () => {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
